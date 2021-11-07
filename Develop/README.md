1bb9750c5beb4cbc199d6e7c57cf920cdb934a59
Matt Thurber <mthurber@Matts-MacBook-Pro-2.local>
2021-11-07 10:29:34 -0700

    Adding updated files

---
+++ Develop/README.md
@@ -0,0 +1,53 @@
+# undefined
+![GitHub license](https://img.shields.io/badge/license-undefined-blue.svg)
+
+## Description
+
+undefined
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+undefined
+```
+
+## Usage
+
+undefined
+
+## License
+
+This project is licensed under the undefined license.
+  
+## Contributing
+
+undefined
+
+## Tests
+
+To run tests, run the following command:
+
+```
+undefined
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at something. You can find more of my work at [something](https://github.com/something/).
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require ("fs");
+const path = require ("path");
+// TODO: Include packages needed fr this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+          }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
+  // Function to initialize app
+  function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+      console.log('Generating README...');
+      writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+    });
+  }
 
 // Function call to initialize app
 init();
