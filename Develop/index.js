5d6cd421870456b63e334d1039ef07f72555dbca
Matt Thurber <mthurber@Matts-MacBook-Pro-2.local>
2021-11-23 16:31:57 -0700

    pushing new read me

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require ("fs");
 const path = require ("path");
+const Choices = require("inquirer/lib/objects/choices");
 // TODO: Include packages needed fr this application
 
 // TODO: Create an array of questions for user input
@@ -15,7 +16,25 @@ const questions = [
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
-          }
+          },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+          },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?',
+          },
+        {
+            type: "list",
+            name: 'license',
+            message: 'What is the license of your project?',
+            choices: ['MIT', 'Apache', 'BSD', 'GPL', 'None']
+            ,
+          },
+
 ];
 
 // TODO: Create a function to write README file
